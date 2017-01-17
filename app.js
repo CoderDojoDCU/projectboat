8e74d424a2d28cc6fe0b7f2449fb9792653a2f2f
Noel King <nking@vsware.ie>
2017-01-17 23:14:27 +0000

    Added search tweet functionality

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-var Twit = require('twit')
+var tweetreader = require('./lib/tweets/readtweets');
 
-var projectBoatTwitter = new Twit({
-  consumer_key:         '',
-  consumer_secret:      '',
-  access_token:         '',
-  access_token_secret:  '',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-});
-
-projectBoatTwitter.get('search/tweets', { q: 'projectboat since:2012-04-01', count: 100 }, function(err, data, response) {
-  console.log(data)
-});
+tweetreader.read("COOLESTPROJECTS");
---
+++ config/twit.js
@@ -0,0 +1,10 @@
+var Twit = require('twit');
+
+var twitter = new Twit({
+  consumer_key:         process.env['consumer_key'],
+  consumer_secret:      process.env['consumer_secret'],
+  access_token:         process.env['access_token'],
+  access_token_secret:  process.env['access_token_secret'],
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+});
+module.exports = twitter
